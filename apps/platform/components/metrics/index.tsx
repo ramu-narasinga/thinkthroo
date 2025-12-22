ca21b693846a39c9bbcaa08485d550fd9d175adc
Tarun-Masapuram <tarun.masapuram@gmail.com>
2025-12-22 15:59:41 +0530

    [#48] removed the import

--- apps/platform/components/metrics/index.tsx
+++ apps/platform/components/metrics/index.tsx
@@ -1,10 +1,8 @@
 // https://medium.com/@victorhcharry/short-how-to-integrate-google-analytics-and-microsoft-clarity-with-nextjs-6174952f218c
-import MicrosoftClarity from "./ms-clarity";
 import Umami from "./umami";
 
 const Metrics = () => (
   <>
-    <MicrosoftClarity />
     <Umami />
   </>
 );
