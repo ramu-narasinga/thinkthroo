f365437502887634e7678719f1f0a3ccf1baa35d
Tarun-Masapuram <tarun.masapuram@gmail.com>
2026-03-23 22:48:16 +0530

    [#166] removed the chunking related

--- apps/platform/server/routers/lambda/index.ts
+++ apps/platform/server/routers/lambda/index.ts
@@ -1,12 +1,10 @@
 import { router } from '@/lib/trpc/lambda';
-import { chunkRouter } from './chunk';
 import { documentRouter } from './document';
 import { organizationRouter } from './organization';
 import { installationRouter } from './installation';
 import { courseProgressRouter } from './courseProgress';
 
 export const lambdaRouter = router({
-  chunk: chunkRouter,
   document: documentRouter,
   organization: organizationRouter,
   installation: installationRouter,
